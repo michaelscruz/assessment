c6ac2e4e5a7758693bf9371bd49396aa77025dbc
Michael Cruz <michaelscruz@yahoo.com>
2016-01-18 17:46:27 -0500

    Create basic user specs. Add validations for first_name and last_name. Add fields for these to the registration views.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,11 @@
 FactoryGirl.define do
+  
   factory :user do
-    
+    sequence(:first_name) { |n| "Person#{n}" }
+    sequence(:last_name) { |n| "Someone#{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com" }
+    password "password"
+    password_confirmation "password"
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryGirl.create :user }
+
+  subject { user }
+
+  it { should respond_to :first_name }
+  it { should respond_to :last_name }
+  it { should respond_to :email }
+  it { should respond_to :password }
+  it { should respond_to :password_confirmation }
+
+  it { should be_valid }
+
+  describe "with an invalid email address" do
+
+    describe "with no email address" do
+      before { user.email = " " }
+
+      it { should_not be_valid }
+    end
+
+    describe "with no @ symbol" do
+      before { user.email = "userexample.com" }
+
+      it { should_not be_valid }
+    end
+
+  end
+
+  describe "with no first name" do
+    before { user.first_name = " " }
+
+    it { should_not be_valid }
+  end
+
+  describe "with no last name" do
+    before { user.last_name = " " }
+
+    it { should_not be_valid }
+  end
+  
 end
