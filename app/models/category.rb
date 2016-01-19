17985553fe33c8fecea0603ed175754025b639dd
Michael Cruz <michaelscruz@yahoo.com>
2016-01-19 11:01:00 -0500

    Add Category specs.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
 class Category < ActiveRecord::Base
+  has_many :questions
+  has_many :category_reports
+
+  validates_presence_of :name
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:category) { FactoryGirl.create :category }
+
+  subject { category }
+
+  it { should respond_to :name }
+  it { should respond_to :questions }
+  it { should respond_to :category_reports }
+
+  it { should be_valid }
+
+  describe "when name is blank" do
+    before { category.name = " " }
+
+    it { should_not be_valid }
+  end
 end
