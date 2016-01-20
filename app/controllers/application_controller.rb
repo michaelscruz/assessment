3a24bb4f7027498596b2535eef9c8df0cdc609c0
Michael Cruz <michaelscruz@yahoo.com>
2016-01-20 14:13:15 -0500

    Add before_action to application to set the account and redirect if needed.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :redirect_if_needed
+  before_action :set_account
 
-  def redirect_if_needed
+  def set_account
     redirect_to "http://oakrootassessments.com" if request.domain == "oakroot.net"
+
+    if request.subdomain.in? ["assessment", "www"]
+      @account = nil
+    else
+      @account = Account.find_by_subdomain(request.subdomain)
+    end
   end
 end
