df96006966ad02e22dc49c6bb30252823c69584e
Michael Cruz <michaelscruz@yahoo.com>
2016-01-20 20:51:05 -0500

    Add titles to pages.

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "UserPages", type: :request do
+  
+  subject { page }
+
+  describe "signing up" do
+    before do
+      visit root_url
+      click_link "Sign up"
+    end
+
+    it { should have_title "Sign up" }
+
+    describe "filling in fields" do
+      before do
+        fill_in "First name", with: "Bob"
+        fill_in "Last name", with: "Bobbingsly"
+        fill_in "Email", with: "bob@example.com"
+        fill_in "Password", with: "password"
+        fill_in "Password confirmation", with: "password"
+        click_button "Sign up"
+      end
+
+      it { should have_content "Welcome! You have signed up successfully." }
+      it { should have_title "Bob Bobbinsly" }
+    end
+  end
+end
