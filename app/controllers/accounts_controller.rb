fb584b42001efab9ffad60092722e8a6bdc2b78c
Michael Cruz <michaelscruz@yahoo.com>
2016-01-26 11:06:49 -0500

    Write the AccountsController code.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,45 @@
 class AccountsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_account, only: [:edit, :update]
+
+  def new
+    @account = Account.new
+  end
+
+  def edit
+  end
+
+  def create
+    @account = Account.new(account_params)
+    @account.user = current_user
+
+    if @account.save
+      flash[:notice] = "Congrats! #{@account.subdomain} is now your subdomain."
+      if params[:creating_test]
+        redirect_to new_exam_path
+      else
+        redirect_to @account.user
+      end
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @account.update(account_params)
+      redirect_to :back, notice: "Your account has been updated successfully."
+    else
+      render :edit
+    end
+  end
+
+  private
+
+    def account_params
+      params.require(:account).permit(:name, :subdomain)
+    end
+
+    def set_account
+      @account = Account.find_by(user_id: current_user.id)
+    end
 end
