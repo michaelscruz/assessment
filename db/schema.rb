9bac00c16565f2f5425e51117f756cef5443bc13
Michael Cruz <michaelscruz@yahoo.com>
2016-02-03 14:06:04 -0500

    Migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160120221307) do
+ActiveRecord::Schema.define(version: 20160203190431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,17 @@ ActiveRecord::Schema.define(version: 20160120221307) do
   end
 
   add_index "category_reports", ["category_id"], name: "index_category_reports_on_category_id", using: :btree
+
+  create_table "category_scores", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "category_id"
+    t.integer  "score"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "category_scores", ["category_id"], name: "index_category_scores_on_category_id", using: :btree
+  add_index "category_scores", ["user_id"], name: "index_category_scores_on_user_id", using: :btree
 
   create_table "exams", force: true do |t|
     t.string   "name"
