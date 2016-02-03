47beba776e01aca097c31e514018a0ed2287e47e
Michael Cruz <michaelscruz@yahoo.com>
2016-02-03 16:34:17 -0500

    Add CategoryScoresController.

---
+++ app/helpers/category_scores_helper.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: category_scores
+#
+#  id          :integer          not null, primary key
+#  user_id     :integer
+#  category_id :integer
+#  score       :integer          default(0), not null
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
+module CategoryScoresHelper
+end
